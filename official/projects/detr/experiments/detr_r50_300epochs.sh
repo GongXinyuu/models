640ae3d243a184a20978deea397d32ed362deb8a
neogong <xinyu.gong@utexas.edu>
2023-04-14 21:23:53 -0500

    update train & eval commands (batch size)

--- official/projects/detr/experiments/detr_r50_300epochs.sh
+++ official/projects/detr/experiments/detr_r50_300epochs.sh
@@ -21,23 +21,32 @@ python3 official/projects/detr/train.py \
 
 
 # train & eval, bs 48
-# task.train_data.global_batch_size=64,task.validation_data.global_batch_size=64,trainer.checkpoint_interval=10,trainer.validation_interval=10
+# trainer.checkpoint_interval=10,trainer.validation_interval=10
+#export PARAMS=$PARAMS,task.train_data.global_batch_size=64
+#export PARAMS=$PARAMS,trainer.optimizer_config.learning_rate.stepwise.values='[1e-4,1e-5]'
+#export PARAMS=$PARAMS,task.train_data.global_batch_size=48
+#export PARAMS=$PARAMS,trainer.optimizer_config.learning_rate.stepwise.values='[7.5e-5,7.5e-6]'
+#export PARAMS=$PARAMS,task.train_data.global_batch_size=40
+#export PARAMS=$PARAMS,trainer.optimizer_config.learning_rate.stepwise.values='[6.25e-5,6.25e-6]'
+#export PARAMS=$PARAMS,task.train_data.global_batch_size=32
+#export PARAMS=$PARAMS,trainer.optimizer_config.learning_rate.stepwise.values='[5e-5,5e-6]'
+
 export PYTHONPATH="${PYTHONPATH}:/home/neoxygong/project/model_garden"
 
 export TPU_NAME=local
 export PARAMS=task.init_checkpoint="official/projects/detr/weights/resnet50_imagenet/ckpt-28080"
 export PARAMS=$PARAMS,trainer.train_steps=554400
-export PARAMS=$PARAMS,task.train_data.global_batch_size=48
 export PARAMS=$PARAMS,task.validation_data.global_batch_size=64
+export PARAMS=$PARAMS,task.train_data.global_batch_size=32
+export PARAMS=$PARAMS,trainer.optimizer_config.learning_rate.stepwise.values='[5e-5,5e-6]'
 export PARAMS=$PARAMS,trainer.optimizer_config.learning_rate.stepwise.boundaries="[369600]"
-export PARAMS=$PARAMS,trainer.optimizer_config.learning_rate.stepwise.values='[0.000075,0.0000075]'
 export PARAMS=$PARAMS,trainer.preemption_on_demand_checkpoint=False
 export PARAMS=$PARAMS,runtime.distribution_strategy=tpu
 
 python3 official/projects/detr/train.py \
   --experiment=detr_coco \
   --mode=train_and_eval \
-  --model_dir=official/projects/detr/output/detr_r50_300epochs_bs48 \
+  --model_dir=official/projects/detr/output/detr_r50_300epochs_bs32 \
   --tpu=$TPU_NAME \
   --params_override=$PARAMS
 
