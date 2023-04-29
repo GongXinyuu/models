9f9290c53b960398a65990d69cfe155748e40336
neogong <xinyu.gong@utexas.edu>
2023-04-29 14:03:52 -0500

    add dino task

---
+++ official/projects/detr/experiments/dino_r50_50epochs.sh
@@ -0,0 +1,13 @@
+export PYTHONPATH="${PYTHONPATH}:/home/neoxygong/project/model_garden"
+
+export TPU_NAME=local
+export PARAMS=task.init_checkpoint="official/projects/detr/weights/resnet50_imagenet/ckpt-28080"
+export PARAMS=$PARAMS,trainer.preemption_on_demand_checkpoint=False
+export PARAMS=$PARAMS,runtime.distribution_strategy=tpu
+
+python3 official/projects/detr/train.py \
+  --experiment=dino_coco \
+  --mode=train_and_eval \
+  --model_dir=official/projects/detr/output/dino_r50_50epochs_bs32 \
+  --tpu=$TPU_NAME \
+  --params_override=$PARAMS
