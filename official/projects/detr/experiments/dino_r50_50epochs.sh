e893ecab2f5ef636805df774e5aeb795435d2dfa
neogong <xinyu.gong@utexas.edu>
2023-04-30 15:09:59 -0500

    update eval command

--- official/projects/detr/experiments/dino_r50_50epochs.sh
+++ official/projects/detr/experiments/dino_r50_50epochs.sh
@@ -11,3 +11,16 @@ python3 official/projects/detr/train.py \
   --model_dir=official/projects/detr/output/dino_r50_50epochs_bs32 \
   --tpu=$TPU_NAME \
   --params_override=$PARAMS
+
+# eval with model_dir, runnable, 0 AP
+export PYTHONPATH="${PYTHONPATH}:/home/neoxygong/project/model_garden"
+
+export TPU_NAME=local
+export PARAMS=task.validation_data.global_batch_size=64
+export PARAMS=$PARAMS,runtime.distribution_strategy=tpu
+python3 official/projects/detr/train.py \
+  --experiment=dino_coco \
+  --mode=eval \
+  --model_dir=official/projects/detr/output/dino_r50_50epochs_bs32 \
+  --tpu=$TPU_NAME \
+  --params_override=$PARAMS
