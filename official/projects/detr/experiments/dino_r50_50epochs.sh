5295a672316fcfadef22093360f83ef3df99074a
neogong <xinyu.gong@utexas.edu>
2023-05-02 14:44:13 -0500

    update training command

--- official/projects/detr/experiments/dino_r50_50epochs.sh
+++ official/projects/detr/experiments/dino_r50_50epochs.sh
@@ -4,7 +4,8 @@ NUN_QUERIES=300
 export TPU_NAME=local
 export PARAMS=task.init_checkpoint="official/projects/detr/weights/resnet50_imagenet/ckpt-28080"
 export PARAMS=$PARAMS,trainer.preemption_on_demand_checkpoint=False
-export PARAMS=$PARAMS,trainer.summary_interval=50
+export PARAMS=$PARAMS,trainer.validation_interval=5000
+export PARAMS=$PARAMS,task.validation_data.global_batch_size=32
 export PARAMS=$PARAMS,task.model.num_queries=$NUN_QUERIES
 export PARAMS=$PARAMS,task.train_data.max_num_boxes=$NUN_QUERIES
 export PARAMS=$PARAMS,task.validation_data.max_num_boxes=$NUN_QUERIES
